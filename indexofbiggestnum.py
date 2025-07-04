72967b33754ffa4d0f059197f4927d19485e571e
Kunchala Chandrika <chandrikakunchala09@gmail.com>
2025-07-04 22:48:22 +0530

    Create indexofbiggestnum.py

---
+++ indexofbiggestnum.py
@@ -0,0 +1,5 @@
+x = [113,45,78,44,78,34,68,4,7,855,24]
+max_num = max(enumerate(x,start=0))
+print('The index of the largest number is',max_num[0])
+min_num = min(enumerate(x,start=0))
+print('the index of the smallest number is',min_num[0])
